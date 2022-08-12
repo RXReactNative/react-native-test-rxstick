fd2d5a10557f73bd081420009041a30310fab855
srxboys <srxboys@126.com>
2022-08-12 17:21:24 +0800

    [mod] use-rversion -> exports

---
+++ src/re/register_16.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+import { View, AppRegistry, StyleSheet } from 'react-native'
+
+import RXEmitter from 'react-native-rxemitter'
+import StickView from './index'
+
+export default class Register extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hidden: true,
+    }
+  }
+
+  componentDidMount() {
+    RXEmitter.addListener(this, 'pattern', ({ hidden }) => {
+      this.setState({ hidden })
+    })
+  }
+
+  UNSAFE_componentWillUnmount() {
+    RXEmitter.removeLister(this)
+  }
+
+  renderStick() {
+    const { hidden } = this.state
+    if (hidden) return null
+    return <StickView />
+  }
+
+  render() {
+    const { children } = this.props
+    return (
+      <View style={styles.container} pointerEvents='box-none'>
+        {children}
+        {this.renderStick()}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFill,
+    backgroundColor: 'transparent',
+  },
+})
+
+if (!AppRegistry.registerComponentForRXStick) {
+  AppRegistry.registerComponentForRXStick = AppRegistry.registerComponent
+}
+
+AppRegistry.registerComponent = function (appKey, componentProvider) {
+  class RootElement extends Component {
+    render() {
+      const Component = componentProvider()
+      return (
+        <Register>
+          <Component {...this.props} />
+        </Register>
+      )
+    }
+  }
+
+  return AppRegistry.registerComponentForRXStick(appKey, () => RootElement)
+}
--- src/register.js
+++ src/register.js
@@ -1,66 +1,9 @@
-import React, { Component } from 'react'
-import { View, AppRegistry, StyleSheet } from 'react-native'
+import rversion from './utils/rversion'
 
-import RXEmitter from 'react-native-rxemitter'
-import StickView from './index'
-
-export default class Register extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hidden: true,
-    }
-  }
-
-  componentDidMount() {
-    RXEmitter.addListener(this, 'pattern', ({ hidden }) => {
-      this.setState({ hidden })
-    })
-  }
-
-  componentWillUnmount() {
-    RXEmitter.removeLister(this)
-  }
-
-  renderStick() {
-    const { hidden } = this.state
-    if (hidden) return null
-    return <StickView />
-  }
-
-  render() {
-    const { children } = this.props
-    return (
-      <View style={styles.container} pointerEvents='box-none'>
-        {children}
-        {this.renderStick()}
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFill,
-    backgroundColor: 'transparent',
-  },
+const Register = rversion(() => {
+  return require('./re/register_16.js')
+}, () => {
+  return require('./re/register.js')
 })
 
-if (!AppRegistry.registerComponentForRXStick) {
-  AppRegistry.registerComponentForRXStick = AppRegistry.registerComponent
-}
-
-AppRegistry.registerComponent = function (appKey, componentProvider) {
-  class RootElement extends Component {
-    render() {
-      const Component = componentProvider()
-      return (
-        <Register>
-          <Component {...this.props} />
-        </Register>
-      )
-    }
-  }
-
-  return AppRegistry.registerComponentForRXStick(appKey, () => RootElement)
-}
+module.exports = Register
